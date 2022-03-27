f210fe4200f119d5d2b1d5ca5192ebf1ac756cb7
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-03-27 23:50:11 +0530

    Update and rename week2.c to week2Q1.c

--- projects/week2Q1.c
+++ projects/week2Q1.c
@@ -1,4 +1,5 @@
 /* 
+10 March 2022
 1)Given an array of sorted intergers containing few duplicate elements to find duplicate element, to find whether the target is present if it is find the number of copy of the target.
 */
 //usig binary search
