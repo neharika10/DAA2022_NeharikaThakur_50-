9c10085b7116a93ea17d370d143354af0aa8c659
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-03-27 23:59:47 +0530

    Update week2Q1.c

--- projects/week2Q1.c
+++ projects/week2Q1.c
@@ -4,4 +4,41 @@
 */
 //usig binary search
 
-
+#include<stdio.h>
+int main()
+{
+  int ar[20],key,i,j,count=0,n,flag=0;
+  printf("enter size of array\n");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&ar[i]);
+  }
+  printf("enter key to be searched\n");
+  scanf("%d",&key);
+  for(i=0;i<n;i++)
+  {
+    if(ar[i]==key)
+    {   
+      flag++;
+      printf("key found at %d position\n",i+1);
+      for(j=i;j<n;j++)
+      {
+        if(ar[j]==key)
+        {
+          count++;
+        }
+        else
+        {
+             printf("key found %d times",count);
+              break;
+        }
+      }
+      break;
+    }
+  }
+  if(flag==0)
+  {
+    printf("key not found");
+  }
+}
