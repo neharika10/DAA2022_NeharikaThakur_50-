730be3a1848f452e0bf105fc789333f02758ef8a
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-03-27 23:46:16 +0530

    Create week1Q2.c

---
+++ projects/week1Q2.c
@@ -0,0 +1,4 @@
+/*
+Given a sorted array of positive integers, design an alogrithm and implement it using a program 
+to find whether key element is present or not. Also find number of comparisons.
+*/
