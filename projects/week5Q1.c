e408fd6e69886cc7178b91e53cdcb646874115c1
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-04-07 09:36:39 +0530

    Create week5Q1.c

---
+++ projects/week5Q1.c
@@ -0,0 +1,5 @@
+/*
+Given an unsorted array of alphabets containing duplicate elements. Design an algorithm and
+implement it using a program to find which alphabet has maximum number of occurrences and
+print it. (Time Complexity = O(n)) (Hint: Use counting sort)
+*/
