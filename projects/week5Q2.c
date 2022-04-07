3545cf2a1aa3581b07e88d7b149ebb93d9031897
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-04-07 09:37:16 +0530

    Create week5Q2.c

---
+++ projects/week5Q2.c
@@ -0,0 +1,5 @@
+/*
+Given an unsorted array of integers, design an algorithm and implement it using a program to
+find whether two elements exist such that their sum is equal to the given key element. (Time
+Complexity = O(n log n))
+*/
