a4b6c6343f5d221986199a2c1aa9f9029d4bd0ca
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-04-18 22:21:06 +0530

    Update week2Q3.c

--- projects/week2Q3.c
+++ projects/week2Q3.c
@@ -3,13 +3,6 @@ Given an array of nonnegative integers, design an algorithm and a program to cou
 their difference is equal to a given key, K.
 
 */
-
-/*
-Given an array of nonnegative integers, design an algorithm and a program to count the number of pairs of integers such that
-their difference is equal to a given key, K.
-
-*/
-
 #include <stdio.h>
 int main()
 {
