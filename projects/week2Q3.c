955b8c4a6b2216af3f3d3346b049889e2833a15d
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-04-18 22:20:52 +0530

    Update week2Q3.c

--- projects/week2Q3.c
+++ projects/week2Q3.c
@@ -4,34 +4,40 @@ their difference is equal to a given key, K.
 
 */
 
-#include<stdio.h>
+/*
+Given an array of nonnegative integers, design an algorithm and a program to count the number of pairs of integers such that
+their difference is equal to a given key, K.
+
+*/
+
+#include <stdio.h>
 int main()
 {
-  int ar[20],i,j,k,n,count=0,flag=0;
+  int ar[20], i, j, k, n, count = 0, flag = 0;
   printf("enter length of array\n");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
+  scanf("%d", &n);
+  for (i = 0; i < n; i++)
   {
-    scanf("%d",&ar[i]);
+    scanf("%d", &ar[i]);
   }
-  for(i=0;i<n;i++)
+  printf("enter key:");
+  scanf("%d",&k);
+  for (i = 0; i < n; i++)
   {
-    for(j=i+1;j<n;j++)
+    for (j = i + 1; j < n; j++)
     {
-      for(k=j+1;k<n;k++)
+
       {
-        if(ar[i]+ar[j]==ar[k])
+        if (ar[i] - ar[j] == k || ar[j]-ar[i]==k)
         {
           flag++;
           count++;
-          printf("%d pair is of index %d and %d resulting to %d\n",count,i,j,k);
-
-          }
         }
       }
     }
-
-  if(flag==0)
+  }
+  printf("%d pairs",count);
+  if (flag == 0)
   {
     printf("no pair found\n");
   }
