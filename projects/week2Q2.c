081f4cacf723b40e375c501ecee9592bbe7cc6e9
neharika10 <95643524+neharika10@users.noreply.github.com>
2022-04-18 22:04:45 +0530

    Update week2Q2.c

--- projects/week2Q2.c
+++ projects/week2Q2.c
@@ -1,3 +1,7 @@
+/*
+Given a sorted array of positive integers,
+design an algorithm and implement it using a program to find three indices i, j, k such that arr[i] + arr[j] = arr[k].
+*/
 #include<stdio.h>
 int main()
 {
